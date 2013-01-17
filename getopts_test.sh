5cf1c6582438dc35796e07cf240a86e53302b94b
chenbh <chenbinghuilove@sina.com>
2013-01-17 13:21:31 +0800

    add function usage

--- getopts_test.sh
+++ getopts_test.sh
@@ -4,7 +4,15 @@
 #author:chenbinghuilove
 #content: test the option about getopts
 
-while getopts a:b:c:d:f OPTION
+
+usage()
+{
+	echo "usage:getopts_test.sh [option] username";
+	echo "eg:getopts_test.sh -a chen";
+	echo "eg:getopts_test.sh -a chen -b bing -c hui";
+}
+
+while getopts a:b:c:d:f:h OPTION
 do
 	case "$OPTION" in
 	a) echo "your option contain a,with the values is $OPTARG";;
@@ -12,6 +20,7 @@ do
 	c) echo "your option has c,with the values is $OPTARG";;
 	d) echo "your option has d,with the values is $OPTARG";;
 	f) echo "your option has f,with the values is $OPTARG";;
+	h) usage ;;
 	*) echo "unknown option:$OPTARG";;
 	esac
 done
